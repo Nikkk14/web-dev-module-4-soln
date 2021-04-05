29a0a293e57c5688b9a52d4469f84656d8ca5b3b
Nikkk14 <nikhilpanchal.14.np@gmail.com>
2021-04-05 06:32:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
-            byeSpeaker.speak(names[i]);
+            byeName.speak(names[i]);
         } else {
-            helloSpeaker.speak(names[i]);
+            helloName.speak(names[i]);
         }
     }
 })();
